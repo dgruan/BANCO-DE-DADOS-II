ed8c9d99fb8eebd4e2337b3c88d688f70a49aa4f
dgruan <douglasruan27@outlook.com>
2026-08-05 20:20:15 -0300

    Add files via upload

---
+++ 1° lista/Primeiro bd.sql
@@ -0,0 +1,35 @@
+create database lista01;
+use lista01;
+
+CREATE TABLE editora (
+cod_editora INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(45) NOT NULL,
+endereco VARCHAR(45)
+);
+
+CREATE TABLE autor (
+cod_autor INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(45) NOT NULL,
+sexo CHAR(1) NOT NULL,
+data_nascimento DATE NOT NULL
+);
+
+CREATE TABLE livro (
+cod_livro INT AUTO_INCREMENT PRIMARY KEY,
+isbn VARCHAR(45),
+titulo VARCHAR(45),
+num_edicao INT,
+preco FLOAT,
+editora_cod_editora INT,
+FOREIGN KEY (editora_cod_editora) REFERENCES editora(cod_editora)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+);
+
+CREATE TABLE livro_autor (
+cod_livro INT,
+cod_autor INT,
+PRIMARY KEY(cod_livro, cod_autor),
+FOREIGN KEY (cod_livro) REFERENCES livro(cod_livro) ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY (cod_autor) REFERENCES autor(cod_autor) ON UPDATE CASCADE ON DELETE CASCADE
+);
